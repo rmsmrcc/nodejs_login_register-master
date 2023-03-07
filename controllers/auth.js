46f8d1315a16235518c6bc1cc3db6562e6f558ba
rmsmrcc <rmsmrcc@gmail.com>
2023-03-07 15:56:21 +0800

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,130 @@
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { promisify } = require('util');
+const { encrypt } = require('text-encrypter');
+const shift = 3; // Default is 1
+const ignoreSpecialCharacters = false; // Default is true
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+});
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if( !email || !password ) {
+      return res.status(400).render('login', {
+        message: 'Please provide an email and password'
+      })
+    }
+
+    db.query('SELECT * FROM user WHERE email = ?', [email], async (error, results) => {
+      console.log(results);
+      if( !results || !(await bcrypt.compare(password, results[0].password)) ) {
+        res.status(401).render('login', {
+          message: 'Email or Password is incorrect'
+        })
+      } else {
+        const id = results[0].id;
+
+        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        console.log("The token is: " + token);
+
+        res.cookie('jwt', token);
+        res.status(200).redirect("/");
+      }
+
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.register = (req, res) => {
+  console.log(req.body);
+
+  const { email, password, passwordConfirm, role} = req.body;
+
+  db.query('SELECT email FROM user WHERE email = ?', [email], async (error, results) => {
+    if(error) {
+      console.log(error);
+    }
+
+    if( results.length > 0 ) {
+      return res.render('register', {
+        message: 'That email is already in use'
+      })
+    } else if( password !== passwordConfirm ) {
+      return res.render('register', {
+        message: 'Passwords do not match'
+      });
+    }
+
+    let encryptedText = encrypt(password, shift, ignoreSpecialCharacters);
+    console.log(encryptedText);
+
+    db.query('INSERT INTO user SET ?', { email: email, password: encryptedText ,role: role}, (error, results) => {
+      if(error) {
+        console.log(error);
+      } else {
+        console.log(results);
+        return res.render('register', {
+          message: 'User registered'
+        });
+      }
+    })
+  });
+
+}
+
+exports.isLoggedIn = async (req, res, next) => {
+  //  console.log(req.cookies);
+  if( req.cookies.jwt) {
+    try {
+      //1) verify the token
+      const decoded = await promisify(jwt.verify)(req.cookies.jwt,
+      process.env.JWT_SECRET
+      );
+
+      console.log(decoded);
+
+      //2) Check if the user still exists
+      db.query('SELECT * FROM user WHERE id = ?', [decoded.id], (error, result) => {
+        console.log(result);
+
+        if(!result) {
+          return next();
+        }
+
+        req.user = result[0];
+        console.log("user is")
+        console.log(req.user);
+        return next();
+
+      });
+    } catch (error) {
+      console.log(error);
+      return next();
+    }
+  } else {
+    next();
+  }
+}
+
+exports.logout = async (req, res) => {
+  res.cookie('jwt', 'logout', {
+    expires: new Date(Date.now() + 2*1000),
+    httpOnly: true
+  });
+
+  res.status(200).redirect('/');
+}
